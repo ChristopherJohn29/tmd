d2571b90f5eeaed46eb4f195feda145fef085128
Nikko <nifernandez.beatifico@firstlead.con.ph>
2018-11-23 15:54:36 +0800

    - add logic on patients CPO adding data if it's certification / re-certification

---
+++ application/controllers/patient_management/CPO.php
@@ -0,0 +1,59 @@
+<?php
+
+class CPO extends MY_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->model(array(
+			'patient_management/cpo_model'
+		));
+	}
+
+	public function index()
+	{
+		$this->check_permission('view_cpo');
+
+		$params = [
+			'table_key' => 'ptcpo_dateCreated',
+			'order_type' => 'DESC',
+			'records_model' => 'cpo_model'
+		];
+
+		$page_data['records'] = parent::get_latest_records($params);
+	}
+
+	public function add()
+	{
+		$this->check_permission('add_cpo');
+
+		$page_data['ptcpo_status'] = $this->cpo_model->generate_status();
+	}
+
+	public function edit(string $ptcpo_patientID)
+	{
+		$this->check_permission('edit_cpo');
+
+		$params = [
+			'table_key' => 'ptcpo_patientID',
+        	'record_key' => $ptcpo_patientID,
+        	'record_table' => 'cpo_model'
+		];
+
+		$page_data['record'] = parent::get_record($params);
+	}
+
+	public function save(string $ptcpo_patientID = '')
+	{
+		$this->check_permission('add_cpo');
+
+		$params = [
+			'record_id' => $ptcpo_patientID,
+			'table_key' => 'ptcpo_patientID',
+			'save_model' => 'cpo_model',
+			'redirect_url' => 'patient_management/cpo/details/'
+		];
+
+		parent::save($params);
+	}
+}
---
+++ application/models/patient_management/CPO_model.php
@@ -0,0 +1,37 @@
+<?php
+
+class CPO_model extends My_Model {
+	protected $table_name = 'patient_CPO';
+
+	private $certification_status = 'Certification';
+	private $re_certification_status= 'Re-Certification';
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function prepare_data() : array
+	{
+		return [
+			'ptcpo_patientID' => $this->uri->segment(3),
+			'ptcpo_period' => $this->input->post('ptcpo_period'),
+			'ptcpo_dateSigned' => $this->input->post('ptcpo_dateSigned'),
+			'ptcpo_firstMonthCPO' => $this->input->post('ptcpo_firstMonthCPO'),
+			'ptcpo_secondMonthCPO' => $this->input->post('ptcpo_secondMonthCPO'),
+			'ptcpo_thirdMonthCPO' => $this->input->post('ptcpo_thirdMonthCPO'),
+			'ptcpo_dischargeDate' => $this->input->post(''),
+			'ptcpo_dateBilled' => $this->input->post(''),
+			'ptcpo_status' => $this->input->post('ptcpo_status')
+		];
+	}
+
+	public function generate_status() : string
+	{
+		$query = $this->db->get($this->table_name);
+
+		$res = $query->result_array();
+
+		return count($res) ? $this->certification_status : $this->re_certification_status;
+	}
+}
