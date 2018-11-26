359d00d17ba64472a2e03af818f83ea78ebeb99a
Nikko <nifernandez.beatifico@firstlead.con.ph>
2018-11-26 17:40:47 +0800

    - all base_url to header template

--- application/views/commons/header.php
+++ application/views/commons/header.php
@@ -18,13 +18,13 @@
 			
 			<li class="dropdown user user-menu">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-					<img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+					<img src="{{ base_url }}dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
 					<span class="hidden-xs">{{ user_fullname }}</span>
 				</a>
 				<ul class="dropdown-menu">
 					<!-- User image -->
 					<li class="user-header">
-						<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+						<img src="{{ base_url }}dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
 						<p>
 							{{ user_fullname }}
