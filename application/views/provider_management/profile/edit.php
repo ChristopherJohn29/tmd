41b45af11619609799149274b64bdb25415dd3e3
Nikko <nifernandez.beatifico@firstlead.con.ph>
2018-11-26 17:41:24 +0800

    - change all update files to edit filename

---
+++ application/views/provider_management/profile/edit.php
@@ -0,0 +1,206 @@
+{% extends "main.php" %}
+
+{% set page_title = 'Update Provider' %}
+
+{% block content %}
+	<div class="row">
+
+		  <div class="col-lg-8">
+          <div class="box">
+          
+            <div class="box-header with-border">
+              <h3 class="box-title">Update Provider</h3>
+            </div>
+            <!-- /.box-header -->
+				
+				<!-- form start -->
+	            <form class="form-horizontal">
+	              <div class="box-body">
+	                
+	                <div class="form-group xrx-form-subheader">
+	              		<div class="col-sm-3"></div>
+	              		<div class="col-sm-7"><p class="lead">Personal Information</p></div>
+	              	</div>
+	              	
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">First Name</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Last Name</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Phone No</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Email</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Address Line 1</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Address Line 2</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Date of Birth</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                  
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Languages</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Areas</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group xrx-form-subheader">
+	              		<div class="col-sm-3"></div>
+	              		<div class="col-sm-7"><p class="lead">Credentials</p></div>
+	              	</div>
+	              	
+	              	<div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">National Provider Identifier</label>
+	                  	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">DEA Registration Number</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">License</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group xrx-form-subheader">
+	              		<div class="col-sm-3"></div>
+	              		<div class="col-sm-7"><p class="lead">Rates</p></div>
+	              	</div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Initial Visit</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Follow-up Visit</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">AW</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">ACP</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">No Show Patients</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Others</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group">
+	                  <label for="" class="col-sm-3 control-label">Mileage</label>
+	                  
+	                  <div class="col-sm-7">
+	                  	<input type="text" class="form-control" id="" placeholder="">
+	                  </div>
+	                </div>
+	                
+	                <div class="form-group xrx-btn-handler">
+	              		<div class="col-sm-3"></div>
+	              		<div class="col-sm-7">
+		              		<button type="button" class="btn btn-primary xrx-btn">
+								<i class="fa fa-check"></i> Update
+							</button>
+	              		</div>
+	              	</div>
+	                
+	              </div>
+	              
+	            </form>
+            
+          </div>
+          <!-- /.box -->
+
+      	</div>
+
+      </div>
+{% endblock %}
