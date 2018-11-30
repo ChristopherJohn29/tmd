af058591a54cda3d81e07beaae54ab65c8738f3f
Jayson Arcayna <jaysonarcayna@ACA80107.ipt.aol.com>
2018-11-30 13:20:57 +0800

    - update add user file

--- application/views/user_management/profile/add.php
+++ application/views/user_management/profile/add.php
@@ -1,11 +1,20 @@
 {% extends "main.php" %}
 
+{% 
+  set scripts = [
+    'bower_components/select2/dist/js/select2.full.min',
+    'plugins/input-mask/jquery.inputmask',
+    'plugins/input-mask/jquery.inputmask.date.extensions',
+    'plugins/input-mask/jquery.inputmask.extensions'
+  ]
+%}
+
 {% set page_title = "Add User" %}
 
 {% block content %}
 	<div class="row">
 
-		  <div class="col-lg-8">
+		  <div class="col-lg-6">
           <div class="box">
           
             <div class="box-header with-border">
@@ -14,73 +23,120 @@
             <!-- /.box-header -->
 				
 				<!-- form start -->
-	            <form class="form-horizontal">
-	              <div class="box-body">
-	              
-	              	<div class="form-group xrx-form-subheader">
-	              		<div class="col-sm-3"></div>
-	              		<div class="col-sm-7"><p class="lead">User Information</p></div>
-	              	</div>
-	                
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">First Name</label>
-	                  
-	                  <div class="col-sm-7">
-	                  	<input type="text" class="form-control" id="" placeholder="">
-	                  </div>
-	                </div>
-	                
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">Last Name</label>
-	                  
-	                  <div class="col-sm-7">
-	                  	<input type="text" class="form-control" id="" placeholder="">
-	                  </div>
-	                </div>
-	                
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">Date of Birth</label>
-	                  
-	                  <div class="col-sm-7">
-	                  	<input type="text" class="form-control" id="" placeholder="">
-	                  </div>
-	                  
-	                </div>
-	                
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">Email</label>
-	                  
-	                  <div class="col-sm-7">
-	                  	<input type="text" class="form-control" id="" placeholder="">
-	                  </div>
-	                  
-	                </div>
-	                
-	                <div class="form-group">
-	                  <label for="" class="col-sm-3 control-label">Type of Access</label>
-	                  
-	                  <div class="col-sm-7">
-	                  	<select class="form-control select2" style="width: 100%;">
-		                  <option selected="selected">Normal user</option>
-		                  <option>Administrator</option>
-		                </select>
-	                  </div>
-	                </div>
-	                
-	                
-	                
-	                <div class="form-group xrx-btn-handler">
-	              		<div class="col-sm-3"></div>
-	              		<div class="col-sm-7">
-		              		<button type="button" class="btn btn-primary xrx-btn">
-								<i class="fa fa-check"></i> Add User
-							</button>
-	              		</div>
-	              	</div>
-	                
-	              </div>
-	              
-	            </form>
+	            <div class="row">
+					<div class="col-lg-12">
+						<div class="box-body">
+						
+							<form class="xrx-form">
+							
+								<div class="row">
+								
+									<div class="col-md-12">
+										<p class="lead">Personal Information</p>
+									</div>
+									
+									<div class="col-md-6 form-group">
+									
+										<label class="control-label">First Name</label>
+										<input type="text" class="form-control" id="" placeholder="" required>
+										
+									</div>
+									
+									<div class="col-md-6 form-group">
+									
+										<label class="control-label">Last Name</label>
+										<input type="text" class="form-control" id="" placeholder="" required>
+										
+									</div>
+                                    
+                                    <div class="col-md-12 subheader">
+										<p class="lead">Basic Information</p>
+									</div>
+                                    
+                                    <div class="col-md-6 form-group">
+									
+										<label class="control-label">Date of Birth</label>
+										<input type="text" class="form-control" data-inputmask="'alias': 'mm/dd/yyyy'" data-mask required>
+										
+									</div>
+                                    
+                                    <div class="col-md-6 form-group">
+									
+										<label class="control-label">Gender</label>
+										<select class="form-control" style="width: 100%;" name="gender" id="dob" required>
+						                  <option selected="selected">Male</option>
+						                  <option>Female</option>
+						                </select>
+										
+									</div>
+                                    
+                                    <div class="col-md-12 subheader">
+										<p class="lead">Contact Information</p>
+									</div>
+                                    
+                                    <div class="col-md-6 form-group">
+									
+										<label class="control-label">Phone</label>
+										<input type="text" class="form-control" id="" placeholder="" required>
+										
+									</div>
+									
+									<div class="col-md-6 form-group">
+									
+										<label class="control-label">Email</label>
+										<input type="text" class="form-control" id="" placeholder="" required>
+										
+									</div>
+                                    
+                                    <div class="col-md-12 form-group">
+									
+										<label class="control-label">Address</label>
+										<input type="text" class="form-control" id="" placeholder="" required>
+										
+									</div>
+                                    
+                                    <div class="col-md-12 subheader">
+										<p class="lead">Access Details</p>
+									</div>
+                                    
+                                    <div class="col-md-12 form-group">
+									
+										<label class="control-label">Account Type</label>
+										
+										<select class="form-control" style="width: 100%;" required>
+											<option selected="selected">Administrator</option>
+											<option>Normal</option>
+										</select>
+										
+									</div>
+									
+									<div class="col-md-6 form-group">
+									
+										<label class="control-label">Password</label>
+										<input type="password" class="form-control" id="" placeholder="" required>
+										
+									</div>
+									
+									<div class="col-md-6 form-group">
+									
+										<label class="control-label">Confirm Password</label>
+										<input type="password" class="form-control" id="" placeholder="" required>
+										
+									</div>
+									
+									<div class="col-md-12 form-group xrx-btn-handler">
+					              		<button type="button" class="btn btn-primary xrx-btn">
+											<i class="fa fa-check"></i> Add User
+										</button>
+					              	</div>
+								
+								</div>
+								
+							</form>
+							
+						</div>
+					</div>
+				</div>
             
           </div>
           <!-- /.box -->
