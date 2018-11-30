22fee49773d8e9e27eaaa1a94ce1b90a76c9dc0d
Jayson Arcayna <jaysonarcayna@ACA80107.ipt.aol.com>
2018-11-30 13:21:45 +0800

    - update list users file

--- application/views/user_management/profile/list.php
+++ application/views/user_management/profile/list.php
@@ -17,30 +17,36 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-            <table id="all-patient-list" class="table no-margin table-hover">
-              <thead>
-                <tr>
-                  <th>Email</th>
-                  <th>Full Name</th>
-                  <th>Type of Access</th>
-                  <th width="80px">Actions</th>
-                </tr>
-              </thead>
-                        
-              <tbody>
+            <div class="table-responsive">
+                <table id="all-patient-list" class="table no-margin table-hover">
 
+                  <thead>
+                    <tr>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Birthday</th>
+                        <th>Account Type</th>
+                        <th width="120px">Actions</th>
+                    </tr>
+                  </thead>
 
-                  <tr>
-                    <td>jayson@email.com</td>
-                    <td>Jayson Arcayna</td>
-                    <td></td>
-                    <td><a href="{{ site_url('user_management/profile/edit/1') }}" title="Edit"><span class="label label-primary">Update</span></a></td>
-                  </tr>
+                  <tbody>
+                    <tr>
+                        <td>Jayson Arcayna</td>
+                        <td>jayson.arcayna@gmail.com</td>
+                        <td>+63 9158826474</td>
+                        <td>10/28/1979</td>
+                        <td>Administrator</td>
+                        <td>
+                            <a href="{{ site_url('user_management/profile/edit/1') }}" title="Edit"><span class="label label-primary">Update</span></a>
+                            <a href="#"><span class="label label-primary">Delete</span></a>
+                        </td>
+                    </tr>
+                  </tbody>
 
-                
-                
-              </tbody>
-            </table>
+                </table>
+            </div>
         </div>
         <!-- /.box-body -->
       </div>
