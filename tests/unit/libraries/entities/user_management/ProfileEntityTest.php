3518f2e344acb73da7b487a445a3eaeedffc5087
Nikko <nifernandez.beatifico@firstlead.con.ph>
2018-11-30 12:37:29 +0800

    - remote all unnecessary unit test

--- tests/unit/libraries/entities/user_management/ProfileEntityTest.php
+++ tests/unit/libraries/entities/user_management/ProfileEntityTest.php
@@ -5,7 +5,6 @@ use  Mobiledrs\entities\user_management\Profile_entity;
 class ProfileEntityTest extends \Codeception\Test\Unit
 {
     private $profile_entity = null;
-    private $encrypted_pass = '$2y$10$pDdFRdvB.ze4q6pB/s5cFOw4WmdL9kzDf9KZHssajRnfBPqrB2jBG';
     
     protected function _before()
     {
@@ -15,31 +14,6 @@ class ProfileEntityTest extends \Codeception\Test\Unit
     protected function _after()
     {
         unset($this->profile_entity);
-    }
-
-    public function testSettingProperties()
-    {
-        $this->profile_entity->user_id = '1';
-        $this->profile_entity->user_firstname = 'Nikkolai';
-        $this->profile_entity->user_lastname = 'Fernandez';
-        $this->profile_entity->user_email = 'nikkolaifernandez14@gmail.com';
-        $this->profile_entity->user_dateCreated = '2018-11-30';
-        $this->profile_entity->user_password = $this->encrypted_pass;
-        $this->profile_entity->user_roleID = 1;
-        $this->profile_entity->user_dateOfBirth = '1992-11-14';
-        $this->profile_entity->roles_id = 1;
-        $this->profile_entity->roles_name = 'Super Administrator';
-
-        $this->assertEquals('1', $this->profile_entity->user_id);
-        $this->assertEquals('Nikkolai', $this->profile_entity->user_firstname);
-        $this->assertEquals('Fernandez', $this->profile_entity->user_lastname);
-        $this->assertEquals('nikkolaifernandez14@gmail.com', $this->profile_entity->user_email);
-        $this->assertEquals('2018-11-30', $this->profile_entity->user_dateCreated);
-        $this->assertEquals($this->encrypted_pass, $this->profile_entity->user_password);
-        $this->assertEquals(1, $this->profile_entity->user_roleID);
-        $this->assertEquals('1992-11-14', $this->profile_entity->user_dateOfBirth);
-        $this->assertEquals('1', $this->profile_entity->roles_id);
-        $this->assertEquals('Super Administrator', $this->profile_entity->roles_name);
     }
 
     public function testGetFullName()
