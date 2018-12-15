af4f7d14be213dbf3dd614eb17c4f2d099a363cf
Nikkolai Fernandez <nikkolaifernandez14@gmail.com>
2018-12-14 22:51:42 -0800

    - remove unused code

--- application/views/patient_management/profile/details.php
+++ application/views/patient_management/profile/details.php
@@ -187,61 +187,7 @@
                                 </div>
                                 {% endfor %}
                             </div>
-                            
-<!--
-             				<div class="table-responsive">
-             				   <table class="table no-margin table-hover">
-								<thead>
-									<tr>
-										<th>Type of Visit</th>
-										<th>Provider</th>
-										<th>Date of Service</th>
-										<th>Deductible</th>
-										<th>AW/IPPE</th>
-                                        <th>Performed</th>
-										<th>Date Billed</th>
-										<th>ACP</th>
-										<th>Diabetes</th>
-										<th>Tobacco</th>
-                                        <th>Others</th>
-										<th>ICD-Code Diagnoses</th>
-										<th>Visit Billed</th>
-                                        <th width="90px">Actions</th>
-									</tr>
-								</thead>
-								
-								<tbody>
-
-
-                                    {% for transaction in transactions %}
-
-    									<tr>
-    										<td>{{ transaction.tov_name }}</td>
-    										<td>{{ transaction.get_provider_fullname() }}</td>
-    										<td>{{ transaction.pt_dateOfService }}</td>
-    										<td>{{ transaction.pt_deductible }}</td>
-    										<td>{{ transaction.pt_aw_ippe_code }}</td>
-                                            <td> </td>
-                                            <td>{{ transaction.pt_aw_ippe_date }}</td>
-                                            <td>{{ transaction.get_selected_choice_format(transaction.pt_acp) }}</td>
-                                            <td>{{ transaction.get_selected_choice_format(transaction.pt_diabetes) }}</td>
-                                            <td>{{ transaction.get_selected_choice_format(transaction.pt_tobacco) }}</td>
-                                            <td> </td>
-                                            <td>{{ transaction.pt_icd10_codes }}</td>
-                                            <td>{{ transaction.pt_dateBilled }}</td>
-                                            <td>
-                                                <a href="{{ site_url("patient_management/transaction/edit/#{ transaction.pt_patientID }/#{ transaction.pt_id }") }}"><span class="label label-primary">Update</span></a>
-                                            </td>
-    									</tr>
-
-                                    {% endfor %}
-
-
-								</tbody>
-							</table>
-                            </div>
--->
-                            
+                           
 							<a href="{{ site_url("patient_management/transaction/add/#{ record.patient_id }") }}" title="">
 								<button type="button" class="btn btn-default">
 									<i class="fa fa-plus"></i> Add Transaction
