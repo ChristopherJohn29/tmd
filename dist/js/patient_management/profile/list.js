c9309f585db77453d03aa7f82d6d0e5d446d7a4d
Nikkolai Fernandez <nikkolaifernandez14@gmail.com>
2018-12-18 08:06:02 -0800

    - add Patient List - only Date of Service and Provider has the sort option function.

--- dist/js/patient_management/profile/list.js
+++ dist/js/patient_management/profile/list.js
@@ -1,10 +1,17 @@
  $(function () {
     $('#all-patient-list').DataTable({
-      'paging'      : true,
-      'lengthChange': true,
-      'searching'   : true,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : false
+      	'paging'      : true,
+      	'lengthChange': true,
+      	'searching'   : true,
+      	'info'        : true,
+      	'autoWidth'   : false,
+  		"columnDefs": [
+	    	{ "orderable": false, "targets": 0 },
+	    	{ "orderable": false, "targets": 1 },
+	    	{ "orderable": false, "targets": 2 },
+	    	{ "orderable": true, "targets": 3 },
+	    	{ "orderable": true, "targets": 4 },
+	    	{ "orderable": false, "targets": 5 }
+		]
     })
   })
