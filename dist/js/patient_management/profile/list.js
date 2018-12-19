739c76c6fdd65dbfcd52aea71d2c190610ad943a
Nikkolai Fernandez <nikkolaifernandez14@gmail.com>
2018-12-19 07:31:35 -0800

    - update DB schema file

--- dist/js/patient_management/profile/list.js
+++ dist/js/patient_management/profile/list.js
@@ -5,13 +5,13 @@
       	'searching'   : true,
       	'info'        : true,
       	'autoWidth'   : false,
-  		"columnDefs": [
-	    	{ "orderable": false, "targets": 0 },
-	    	{ "orderable": false, "targets": 1 },
-	    	{ "orderable": false, "targets": 2 },
-	    	{ "orderable": true, "targets": 3 },
-	    	{ "orderable": true, "targets": 4 },
-	    	{ "orderable": false, "targets": 5 }
-		]
+  		  "columnDefs": [
+  	    	{ "orderable": false, "targets": 0 },
+  	    	{ "orderable": false, "targets": 1 },
+  	    	{ "orderable": false, "targets": 2 },
+  	    	{ "orderable": true, "targets": 3 },
+  	    	{ "orderable": true, "targets": 4 },
+  	    	{ "orderable": false, "targets": 5 }
+		    ]
     })
   })
