bb5108e65dc2d335f7aa965842c16735f2a46d67
Jeca Arcayna <jecaarcayna@Jecas-MacBook-Pro.local>
2018-12-22 10:43:49 +0800

    - Update dashboard content

--- application/views/home.php
+++ application/views/home.php
@@ -166,264 +166,97 @@
       
 </div>
 
-<div class="row">
-    <!-- Left col -->
-    <div class="col-md-6">
-    <!-- TABLE: PATIENT LIST -->
-        <div class="box">
-            
-            <div class="box-header with-border">
-                <h3 class="box-title">List of Providers</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-                
-                <div class="table-responsive">
-                    
-                    <table class="table no-margin table-hover">
-                        
-                        <thead>
-                            <tr>
-                                <th>Provider Name</th>
-                                <th>Phone</th>
-                                <th width="170px">Actions</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            
-                            <tr>
-                                <td>Alexandra Kirtchik</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Henry Barraza</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Shohreh Nourollah</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Lilibeth Ramirez</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Fidelia NNchetam</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Grace Adeagbo</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Kaixuan Luo</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Reynaldo Salcedo</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Zaydee Mercado</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                            <tr>
-                                <td>Katherine Chin</td>
-                                <td>818.909.2599</td>
-                                <td>
-                                    <a href="details-provider.php"><span class="label label-primary">View Details</span></a>
-                                    <a href="update-provider.php" title=""><span class="label label-primary">Update</span></a>
-                                </td>
-                            </tr>
-                            
-                        </tbody>
-                        
-                    </table>
-                    
-                </div>
-            <!-- /.table-responsive -->
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer text-center">
-            <a href="{{ site_url('provider_management/profile/') }}" class="uppercase">View All Providers</a>
-            </div>
-        <!-- /.box-footer -->
-        </div>
-    <!-- /.box -->
-    </div>
-    <!-- /.col -->
-
-    <div class="col-md-6">
-
-
-    <!-- HOME HEALTH CARE -->
-    <div class="box">
+<div class="row"> 
+    
+    <div class="col-lg-12">
         
+        <div class="box">
+      
         <div class="box-header with-border">
-        <h3 class="box-title">List of Home Health Care</h3>
-        <?php include("inc/box-tools.php");?>
+            <h3 class="box-title">Recently Added Route Sheet</h3>
         </div>
-    <!-- /.box-header -->
+        <!-- /.box-header -->
+        
         <div class="box-body">
-
+            
             <div class="table-responsive">
-
-                <table class="table no-margin table-hover">
+                
+                <table id="" class="table no-margin table-hover">
+                    
                     <thead>
-                        
                         <tr>
-                            <th>Home Health Care</th>
-                            <th>Phone</th>
-                            <th width="90">Actions</th>
+                            <th>Date of Service</th>
+                            <th>Provider</th>
+                            <th width="160px">Actions</th>
                         </tr>
-                        
                     </thead>
 
                     <tbody>
                         
                         <tr>
-                            <td>GMO Home Health</td>
-                            <td>+1 22313198786</td>
+                            <td>01/03/2019</td>
+                            <td>Alexandra Kirtchik</td>
                             <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">View Details</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">Update</span></a>
                             </td>
                         </tr>
-
+                        
                         <tr>
-                            <td>Faith and Hope</td>
-                            <td>+1 22313198786</td>
+                            <td>01/03/2019</td>
+                            <td>Alexandra Kirtchik</td>
                             <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">View Details</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">Update</span></a>
                             </td>
                         </tr>
-
+                        
                         <tr>
-                            <td>Divine Care Home Health</td>
-                            <td>+1 22313198786</td>
+                            <td>01/03/2019</td>
+                            <td>Alexandra Kirtchik</td>
                             <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">View Details</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">Update</span></a>
                             </td>
                         </tr>
-
+                        
                         <tr>
-                            <td>White Shield Home Health</td>
-                            <td>+1 22313198786</td>
+                            <td>01/03/2019</td>
+                            <td>Alexandra Kirtchik</td>
                             <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">View Details</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">Update</span></a>
                             </td>
                         </tr>
-
+                        
                         <tr>
-                            <td>Millenium Home Health</td>
-                            <td>+1 22313198786</td>
+                            <td>01/03/2019</td>
+                            <td>Alexandra Kirtchik</td>
                             <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">View Details</span></a>
+                                <a href="details-patient.php"><span class="label label-primary">Update</span></a>
                             </td>
                         </tr>
-
-                        <tr>
-                            <td>Nightingle Home Health</td>
-                            <td>+1 22313198786</td>
-                            <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Prestige Home Health</td>
-                            <td>+1 22313198786</td>
-                            <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Advance Home Care</td>
-                            <td>+1 22313198786</td>
-                            <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Healthy Choice Home Care</td>
-                            <td>+1 22313198786</td>
-                            <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>R &amp; G Home Health Care</td>
-                            <td>+1 22313198786</td>
-                            <td>
-                                <a href="update-home-health.php" title=""><span class="label label-primary">Update</span></a>
-                            </td>
-                        </tr>
-
+                        
+                        
                     </tbody>
                     
                 </table>
-
+                
             </div>
-        <!-- /.table-responsive -->
-
+            
         </div>
-    <!-- /.box-body -->
+        <!-- /.box-body -->
         <div class="box-footer text-center">
-        <a href="{{ site_url('home_health_care_management/profile/') }}" class="uppercase">View All Home Health</a>
+            <a href="{{ site_url('provider_route_sheet_management/route_sheet/') }}">View All Route Sheet</a>
         </div>
-    <!-- /.box-footer -->
+          
         </div>
-    <!-- /.box -->
+
     </div>
-    <!-- /.col -->
+      
 </div>
+
+
 
 {% endblock %}
