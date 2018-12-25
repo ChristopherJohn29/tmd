49c523065a2e4d466d4d0c13e3aed556f565d778
Nikkolai Fernandez <nikkolaifernandez14@gmail.com>
2018-12-25 07:36:45 -0800

    - fix update routesheet error message

--- application/language/english/common_lang.php
+++ application/language/english/common_lang.php
@@ -6,4 +6,4 @@ $lang['success_delete'] = 'Deleted Successfully.';
 $lang['danger_save'] = 'There is a problem while saving the data, please try again later.';
 $lang['danger_delete'] = 'There is a problem while deleting the data, please try again later.';
 $lang['danger_authentication_invalid_user'] = 'Email / Password is invalid.';
-$lang['danger_routesheet_duplication_provider_sameday'] = 'Adding more than one Routesheet of Provider per day is not allowed.';
+$lang['danger_routesheet_duplication_provider_sameday'] = 'Adding more than one Route Sheet for Provider per day is not allowed.';
