a7653f234dabbed880edeaaa0ced1bea882fd339
Nikkolai Fernandez <nikkolaifernandez14@gmail.com>
2018-12-26 11:09:47 -0800

    - fix Patient List- if "No Show" was the last transaction show notes in icd10 column instead

--- application/models/patient_management/Profile_model.php
+++ application/models/patient_management/Profile_model.php
@@ -71,7 +71,7 @@ class Profile_model extends \Mobiledrs\core\MY_Models {
 						'join_table_key' => 'provider.provider_id',
 						'join_table_condition' => '=',
 						'join_table_value' => 'patient_transactions.pt_providerID',
-						'join_table_type' => 'inner'
+						'join_table_type' => 'left'
 					]
 				]
 			];
