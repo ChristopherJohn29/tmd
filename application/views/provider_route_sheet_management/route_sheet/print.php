81ac73b9df1feebee7b69b6e5aaf4c260c256598
Jayson Arcayna <jaysonarcayna@ACA80107.ipt.aol.com>
2018-12-28 17:56:08 +0800

    - Update file

--- application/views/provider_route_sheet_management/route_sheet/print.php
+++ application/views/provider_route_sheet_management/route_sheet/print.php
@@ -9,4 +9,70 @@
  	window.print();
  </script>
 
+<div class="row">
+    <div class="col-md-12">
+      <div class="box">
+
+        <!-- /.box-header -->
+        <div class="box-body">
+
+            <section class="xrx-info">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="name rs">{{ record.get_provider_fullname() }}<small>Provider Name</small></h1>
+                    </div>
+                </div>
+
+                <div class="row spacer-bottom">
+                    <div class="col-md-12">
+                        <h4>Date of Service: {{ record.get_date_format(record.prs_dateOfService) }}</h4>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <p class="lead">Route Sheet</p>
+
+                        <div class="table-responsive">
+                           <table id="" class="table no-margin table-striped route-sheet">
+                            <thead>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Patient's Info</th>
+                                    <th>Home Health Info</th>
+                                    <th>Notes</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                {% for list in lists %}
+
+                                    <tr>
+                                        <td>{{ list.get_combined_time() }}</td>
+                                        <td><p>{{ list.get_patient_fullname() }}<span>{{ list.patient_address }}<br>{{ list.patient_phoneNum }}</span></p></td>
+                                        <td><p>{{ list.hhc_name }}<span>{{ list.hhc_contact_name }}<br>{{ list.hhc_phoneNumber }}</span></p></td>
+                                        <td><p>Type of Visit : {{ list.tov_name }}<span>Other Notes: {{ list.prsl_notes }}</span></p></td>
+                                    </tr>
+
+                                {% endfor %}
+
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                </div>
+            
+            </section>
+
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+      </div>
+
+</div>
+
 {% endblock %}
