317e55a09353f74ab512c0ccbbaa1321638f6413
Nikko <nikkolaifernandez14@gmail.com>
2019-01-02 02:14:30 +0800

    - Request: Patients list page - Have the option to sort by Referral date as well.

--- dist/js/patient_management/profile/list.js
+++ dist/js/patient_management/profile/list.js
@@ -7,7 +7,7 @@
       	'autoWidth'   : false,
   		  "columnDefs": [
   	    	{ "orderable": false, "targets": 0 },
-  	    	{ "orderable": false, "targets": 1 },
+  	    	{ "orderable": true, "targets": 1 },
   	    	{ "orderable": false, "targets": 2 },
   	    	{ "orderable": true, "targets": 3 },
   	    	{ "orderable": true, "targets": 4 },
