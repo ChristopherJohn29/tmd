15df141a988d97489bd263e99f9f1a9e15b33e53
Nikkolai <nikkolaifernandez14@gmail.com>
2019-01-27 01:45:30 +0800

    -requests: fix patient list page sorting function

--- dist/js/patient_management/profile/list.js
+++ dist/js/patient_management/profile/list.js
@@ -1,6 +1,6 @@
  $(function () {
     $('#all-patient-list').DataTable({
-        "order": [],
+        "order": [[2, 'asc']],
         "pageLength": 100,
         'dom': "<'row'<'col-sm-12'<\".toolbar\">>>tp",
         initComplete: function(){
