3a261a79d8417a54d0024795d16e287cbe368bdd
Jayson Arcayna <jaysonarcayna@ACA80107.ipt.aol.com>
2019-02-12 02:48:23 +0800

    - Update file

--- application/views/provider_route_sheet_management/route_sheet/email_template.php
+++ application/views/provider_route_sheet_management/route_sheet/email_template.php
@@ -1,8 +1,10 @@
 <p style="margin-bottom: 22px;">Hi <strong><?php echo $record->get_provider_fullname(); ?></strong>,</p>
-<p>Attached is a copy of your routesheet for the date of <?php echo $record->get_date_format($record->prs_dateOfService); ?>.</p>
-<p>Please take time to review the file and let me know if you have any concerns or questions.</p>
+
+<p>Please review the attached copy of your Route Sheet for the date of <?php echo $record->get_date_format($record->prs_dateOfService); ?>. Please review the file and let me know should you have any concerns or questions.</p>
+
+<p>Thank you!</p>
 <br>
 <p>
 Regards,<br>
-Michelle
+MERI PAPIKYAN
 </p>
--- application/views/provider_route_sheet_management/route_sheet/pdf.php
+++ application/views/provider_route_sheet_management/route_sheet/pdf.php
@@ -1,48 +1,36 @@
 <img src="<?php echo base_url() ?>/dist/img/pdf_header_portrait.png">
 
-<p style="margin:0; padding:0;">
-	<span style="font-size:16px;"><?php echo $record->get_provider_fullname(); ?></span><br>
-	<span style="font-size:8px; color: gray;">PROVIDER NAME</span>
-</p>
-
-<br>
-<br>
-
-<span style="font-size:10px; padding-top:6px">Date of Service:</span> <?php echo $record->get_date_format($record->prs_dateOfService); ?>
-
-<br>
+<p style="margin:0; padding:0;"><span style="font-size:16px;"><?php echo $record->get_provider_fullname(); ?></span><br>
+<span style="font-size:10px; color: gray; padding-top:6px">Date of Service:</span> <?php echo $record->get_date_format($record->prs_dateOfService); ?></p>
 
 <p style="font-size:8px; color: gray;">ROUTE SHEET</p>
-
-<br>
-<br>
 
 <table style="font-size: 7px;padding: 5px;">
 	<thead>
 		<tr>
-			<th bgcolor="#548bb8" style="color: white;border:1px solid #548bb8;">Time</th>
-			<th bgcolor="#548bb8" style="color: white;border:1px solid #548bb8">Patient's Info</th>
-			<th bgcolor="#548bb8" style="color: white;border:1px solid #548bb8">Home Health Info</th>
-			<th bgcolor="#548bb8" style="color: white;border:1px solid #548bb8">Notes</th>
+			<th width="80px" bgcolor="#548bb8" style="color: white;border:1px solid #548bb8; font-weight:bold;">Time</th>
+			<th width="120px" bgcolor="#548bb8" style="color: white;border:1px solid #548bb8; font-weight:bold;">Patient's Info</th>
+			<th width="120px" bgcolor="#548bb8" style="color: white;border:1px solid #548bb8; font-weight:bold;">Home Health Info</th>
+			<th width="200px" bgcolor="#548bb8" style="color: white;border:1px solid #548bb8; font-weight:bold;">Notes</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php foreach($lists as $list): ?>
 			<tr>
-				<td style="border-bottom: 1px solid #d2d6de;">
+				<td width="80px" style="border-bottom: 1px solid #d2d6de;">
 					<?php echo $list->get_combined_time(); ?>		
 				</td>
-				<td style="border-bottom: 1px solid #d2d6de;">
+				<td width="120px" style="border-bottom: 1px solid #d2d6de;">
 					<?php echo $list->patient_name; ?><br>
 					<?php echo $list->patient_address; ?><br>
 					<?php echo $list->patient_phoneNum; ?>
 				</td>
-	            <td style="border-bottom: 1px solid #d2d6de;">
+	            <td width="120px" style="border-bottom: 1px solid #d2d6de;">
 	            	<?php echo $list->hhc_name ?><br>
 	            	<?php echo $list->hhc_contact_name; ?><br>
     				<?php echo $list->hhc_phoneNumber; ?>
 	            </td>
-				<td style="border-bottom: 1px solid #d2d6de;">
+				<td width="200px" style="border-bottom: 1px solid #d2d6de;">
 					Type of Visit : <?php echo $list->tov_name; ?><br>
 					Other Notes: <?php echo $list->prsl_notes; ?>
 				</td>
