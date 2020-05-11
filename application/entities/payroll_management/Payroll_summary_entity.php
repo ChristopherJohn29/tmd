320d5b5edd08a957b9de986fbf312d8ccfe2671f
Nikkolai <nikkolaifernandez14@gmail.com>
2020-05-11 14:59:13 +0800

    update payroll provider

---
+++ application/entities/payroll_management/Payroll_summary_entity.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Mobiledrs\entities\payroll_management;
+
+class Payroll_summary_entity {
+
+	public $id = null;
+	public $provider_id = null;
+	public $from = null;
+	public $to = null;
+	public $total_amount = null;
+	public $mileage = null;
+
+}
