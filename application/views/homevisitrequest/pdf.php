cf416ed31662763a8f48ee1e95a0b8bd5ed29643
Christopher John Gamo <christopherjohngamo@gmail.com>
2022-12-16 23:48:30 +0800

    Update PDF

--- application/views/homevisitrequest/pdf.php
+++ application/views/homevisitrequest/pdf.php
@@ -6,117 +6,53 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>The Mobile Drs - Intake Form</title>
         
-        <style>
-            .content-handler {
-                display: block;
-                width:80%;
-                margin:70px auto 0;
-                }
-            
-            
-            .section-handler {
-                display: block;
-                width: 100%;
-                border: 1px solid #2f357d;
-                border-bottom-left-radius: 8px;
-                border-bottom-right-radius: 8px;
-                }
-            
-            table {
-                font-family: helvetica;
-                width: 80%;
-                margin: 0 auto;
-                font-size: 14px;
-                border: 0;
-                padding: 0;
-                }
-            
-            table tr {
-                padding: 0;
-                }
-            
-            table td {
-                padding: 0 10px;
-                }
-            
-            table td.w-25 {
-                width: 25%;
-                }
-            
-            table td.w-33 {
-                width: 33.3%;
-                }
-            
-            table td.w-50 {
-                width: 50%;
-                }
-            
-            table td.w-75 {
-                width: 75%;
-                }
-            
-            .section-header {
-                border: 0;
-                padding: 0;
-                }
-            
-            .section-name {
-                color: #fff;
-                background-color: #2f357d;
-                text-transform: uppercase;
-                width: 180px;
-                padding: 10px 20px;
-                border-top-left-radius: 8px;
-                border-top-right-radius: 8px;
-                }
-            
-            .form-content {
-                color: #7d7d7d;
-                border: 1px solid #2f357d;
-                border-bottom-left-radius: 8px;
-                border-bottom-right-radius: 8px;
-                padding: 20px;
-                }
-            
-            .bordered {
-                border: 1px solid #7d7d7d;
-                border-radius: 3px;
-                padding: 10px 20px;
-                margin: 5px 0 15px;
-                }
-            
-        </style>
-        
     </head>
     <body>
         
         
-        <table cellpadding="0" cellspacing="0">
+    <table style="font-size: 8px;">
+        <thead>
             <tr>
-                <td class="section-name">Patient Information</td>
-                <td></td>
+                <th width="80px" bgcolor="#2f357d" style="color: white;">Patient Information</th>
+                <th colspan="2" width="440px" style="color: white;"></th>
             </tr>
-        </table>
-        
-        <table class="form-content">            
-            <tr>
-                <td class="" width="75%">
-                    <span>Patient Name</span>
-                    <div class="bordered">asdasd</div>
-                </td>
-                <td class="w-25">
-                    <span>Date of Birth</span>
-                    <div class="bordered">asdasd</div>
-                </td>
-            </tr>
-            
-            <tr>
-                <td class="w-75" colspan="2">
-                    <span>Address</span>
-                    <div class="bordered">asdasd</div>
-                </td>
-            </tr>
-        </table>
+        </thead>
+        <tbody>
+                <tr>
+                    <td colspan="2" width="347px">
+                        Patient Name<br>
+                        asdadasd
+                    </td>
+                    <td width="173px">
+                        Date of Birth<br>
+                        asdasdasdasd
+                    </td>
+
+                </tr>
+                <tr>
+                    <td width="173px">
+                        Gender<br>
+                        asdasdasd
+                    </td>
+                    <td width="174px">
+                        Phone Number<br>
+                        asdasdasd
+                    </td>
+                    <td width="173px">
+                        Languages<br>
+                        asdasda
+                    </td>
+
+                </tr>
+                <tr>
+                    <td colspan="3" width="520px">
+                        Address<br>
+                        asdasdad
+                    </td>
+
+                </tr>
+        </tbody>
+    </table>
         
         
     </body>
