524e6300e4d7d0f02141c7d5682a4d1da7c2354e
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-01-07 11:40:13 +0800

    UpdatE

--- application/controllers/Home_visit_request.php
+++ application/controllers/Home_visit_request.php
@@ -82,57 +82,57 @@ class Home_visit_request extends \Mobiledrs\core\MY_Controller {
 		$data = $this->user_model->fetchHomeVisitRequestById($id);
 
 		$this->load->view('homevisitrequest/pdf', $data[0]);
-		exit;
-
-		// echo "<pre>";
-		// var_dump($data);
-		// echo "</pre>";
 		// exit;
-		$tmpDir = sys_get_temp_dir() . '/';
-		$html = $this->load->view('homevisitrequest/pdf', $data[0], true);
 
-		$this->load->library(['email', 'PDF']);
+		// // echo "<pre>";
+		// // var_dump($data);
+		// // echo "</pre>";
+		// // exit;
+		// $tmpDir = sys_get_temp_dir() . '/';
+		// $html = $this->load->view('homevisitrequest/pdf', $data[0], true);
 
-		$config = array(
-			'wordwrap'  => true,
-			'protocol'  => 'smtp',
-			'smtp_host' => 'ssl://smtp.bizmail.yahoo.com',
-			'smtp_port' => 465,
-			'smtp_user' => 'info@themobiledrs.com',
-			'smtp_pass' => 'ucjemnxyefywdbyw',
-			'mailtype'  => 'html',
-			'newline' 	=> "\r\n"
-		);
+		// $this->load->library(['email', 'PDF']);
 
-		$this->email->initialize($config);
+		// $config = array(
+		// 	'wordwrap'  => true,
+		// 	'protocol'  => 'smtp',
+		// 	'smtp_host' => 'ssl://smtp.bizmail.yahoo.com',
+		// 	'smtp_port' => 465,
+		// 	'smtp_user' => 'info@themobiledrs.com',
+		// 	'smtp_pass' => 'ucjemnxyefywdbyw',
+		// 	'mailtype'  => 'html',
+		// 	'newline' 	=> "\r\n"
+		// );
 
-		$filename = 'Home Visit Form PDF';
+		// $this->email->initialize($config);
+
+		// $filename = 'Home Visit Form PDF';
 		
 
-		$this->pdf->generate_hv($html, $tmpDir . $filename);
+		// $this->pdf->generate_hv($html, $tmpDir . $filename);
 
-		$this->email->from('info@themobiledrs.com', 'The MobileDrs');
-		// $this->email->reply_to('michelle@themobiledrs.com', 'The MobileDrs');
-		$this->email->to('christopherjohngamo@gmail.com');
-		$this->email->subject('Home visit request form');
-		$this->email->message('Attached is a copy of the Home Visit Request form submitted to The Mobile Drs.');
-		$this->email->attach($tmpDir . $filename . '.pdf', 'attachment', $filename . '.pdf');
+		// $this->email->from('info@themobiledrs.com', 'The MobileDrs');
+		// // $this->email->reply_to('michelle@themobiledrs.com', 'The MobileDrs');
+		// $this->email->to('christopherjohngamo@gmail.com');
+		// $this->email->subject('Home visit request form');
+		// $this->email->message('Attached is a copy of the Home Visit Request form submitted to The Mobile Drs.');
+		// $this->email->attach($tmpDir . $filename . '.pdf', 'attachment', $filename . '.pdf');
 
-		$send = $this->email->send();
+		// $send = $this->email->send();
 
 
-		if ($send)
-		{
-			unlink($tmpDir . $filename . '.pdf');
+		// if ($send)
+		// {
+		// 	unlink($tmpDir . $filename . '.pdf');
 			
-			$this->session->set_flashdata('success', $this->lang->line('success_email'));
-		}
-		else
-		{
-			$this->session->set_flashdata('danger', $this->lang->line('danger_email'));	
-		}
+		// 	$this->session->set_flashdata('success', $this->lang->line('success_email'));
+		// }
+		// else
+		// {
+		// 	$this->session->set_flashdata('danger', $this->lang->line('danger_email'));	
+		// }
 
-		redirect('/home_visit_request');
+		// redirect('/home_visit_request');
 	}
 
 }
