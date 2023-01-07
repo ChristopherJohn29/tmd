bc720cfdf9f984b35db2562ea0639dc1b075d50f
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-01-07 11:41:58 +0800

    UpdatE

--- application/controllers/Home_visit_request.php
+++ application/controllers/Home_visit_request.php
@@ -108,28 +108,28 @@ class Home_visit_request extends \Mobiledrs\core\MY_Controller {
 
 		$this->pdf->generate_hv($html, $tmpDir . $filename);
 
-		// $this->email->from('info@themobiledrs.com', 'The MobileDrs');
-		// // $this->email->reply_to('michelle@themobiledrs.com', 'The MobileDrs');
-		// $this->email->to('christopherjohngamo@gmail.com');
-		// $this->email->subject('Home visit request form');
-		// $this->email->message('Attached is a copy of the Home Visit Request form submitted to The Mobile Drs.');
-		// $this->email->attach($tmpDir . $filename . '.pdf', 'attachment', $filename . '.pdf');
+		$this->email->from('info@themobiledrs.com', 'The MobileDrs');
+		// $this->email->reply_to('michelle@themobiledrs.com', 'The MobileDrs');
+		$this->email->to('christopherjohngamo@gmail.com');
+		$this->email->subject('Home visit request form');
+		$this->email->message('Attached is a copy of the Home Visit Request form submitted to The Mobile Drs.');
+		$this->email->attach($tmpDir . $filename . '.pdf', 'attachment', $filename . '.pdf');
 
-		// $send = $this->email->send();
+		$send = $this->email->send();
 
 
-		// if ($send)
-		// {
-		// 	unlink($tmpDir . $filename . '.pdf');
+		if ($send)
+		{
+			unlink($tmpDir . $filename . '.pdf');
 			
-		// 	$this->session->set_flashdata('success', $this->lang->line('success_email'));
-		// }
-		// else
-		// {
-		// 	$this->session->set_flashdata('danger', $this->lang->line('danger_email'));	
-		// }
+			$this->session->set_flashdata('success', $this->lang->line('success_email'));
+		}
+		else
+		{
+			$this->session->set_flashdata('danger', $this->lang->line('danger_email'));	
+		}
 
-		// redirect('/home_visit_request');
+		redirect('/home_visit_request');
 	}
 
 }
